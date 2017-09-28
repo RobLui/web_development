d63a85a0e1b3d2281c939ffd4e6b7312b1aaa5a3
Robbert <robbertluit@hotmail.com>
2017-09-29 00:40:10 +0200

    add reg controller & mockup views

---
+++ database/migrations/2017_09_28_221825_create_registration_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistrationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registration', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->integer('age');
+            $table->string('adress');
+            $table->tinyInteger('housenumber');
+            $table->string('email', 100)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registration');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,23 +17,24 @@
             </div>
 
             <div class="links">
-                <a href="https://laravel.com/docs">Documentation</a>
-                <a href="https://laracasts.com">Laracasts</a>
-                <a href="https://laravel-news.com">News</a>
-                <a href="https://forge.laravel.com">Forge</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
+                <a href="{{ url('competition') }}">Wedstrijd</a>
+                <a href="{{ url('competition/registration') }}">Registratie</a>
+                {{--<a href="https://laracasts.com">Laracasts</a>--}}
+                {{--<a href="https://laravel-news.com">News</a>--}}
+                {{--<a href="https://forge.laravel.com">Forge</a>--}}
+                {{--<a href="https://github.com/laravel/laravel">GitHub</a>--}}
             </div>
 
             {{--@foreach()--}}
-            <div class="col-md-4">
-                xxxxx
-            </div>
-            <div class="col-md-4">
-                xxxxx
-            </div>
-            <div class="col-md-4">
-                xxxxx
-            </div>
+            {{--<div class="col-md-4">--}}
+                {{--xxxxx--}}
+            {{--</div>--}}
+            {{--<div class="col-md-4">--}}
+                {{--xxxxx--}}
+            {{--</div>--}}
+            {{--<div class="col-md-4">--}}
+                {{--xxxxx--}}
+            {{--</div>--}}
             {{--@endforeach--}}
         </div>
 

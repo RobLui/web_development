f6733f828c9bf2ace4c3e1420ba49d49a6f2d526
Robbert <robbertluit@hotmail.com>
2017-10-03 00:34:28 +0200

    add period/emailmanager/winner...s

--- resources/views/Competition/questions/show.blade.php
+++ resources/views/Competition/questions/show.blade.php
@@ -4,22 +4,25 @@
 
     @include("common.messages")
     @include("common.errors")
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <h2>Vraag:</h2>
-            <div class="col-md-4">
-                {{ $questions->category }}
-            </div>
-            <div class="col-md-6">
-                {{ $questions->title }}
-            </div>
-            <div class="col-md-4">
-                {{ $questions->text }}
-            </div>
-            <div class="col-md-6">
-                {{ $questions->difficulty }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <h2>Vraag:</h2>
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        {{ $questions->category }}
+                    </li>
+                    <li class="list-group-item">
+                        {{ $questions->title }}
+                    </li>
+                    <li class="list-group-item">
+                        {{ $questions->text }}
+                    </li>
+                    <li class="list-group-item">
+                        {{ $questions->difficulty }}
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
-
 @endsection
